fe854cb73e646908b0ced131f5af2afd7250a252
Daniil Rose <daniil.rose@posteo.org>
2023-10-23 16:35:57 -0400

    Add chip8 layout and SDL2

---
+++ src/chip8.c
@@ -0,0 +1,5 @@
+//
+// Created by Daniil Rose on 10/23/23.
+//
+
+#include "chip8.h"
--- src/main.c
+++ src/main.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(int argc, char *argv[]) {
+    CHIP8 emulator = {0};
+
+    SDL_Event event;
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+    const uint16_t WINDOW_HEIGHT = 512;
+    const uint16_t WINDOW_WIDTH = 1024;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    while (1) {
+        if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+            break;
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
