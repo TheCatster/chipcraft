90f4ffad9562ee2d3fdb3a4c5c056e5feaaf4326
Daniil Rose <djr6389@psu.edu>
2023-10-28 17:32:50 -0400

    Split graphics functions into separate file

---
+++ src/graphics.c
@@ -0,0 +1,19 @@
+//
+// Created by daniil on 10/28/23.
+//
+
+#include "graphics.h"
+
+/**
+ * @brief Initialize SDL2 for the emulator
+ * @param void
+ * @returns void
+ */
+void initialize_graphics(void) {
+    SDL_Event event;
+    SDL_Renderer *renderer;
+    SDL_Window *window;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer);
+}
---
+++ src/graphics.h
@@ -0,0 +1,12 @@
+//
+// Created by daniil on 10/28/23.
+//
+
+#pragma once
+
+#include <SDL2/SDL.h>
+
+#define WINDOW_HEIGHT 320
+#define WINDOW_WIDTH 640
+
+void initialize_graphics(void);
