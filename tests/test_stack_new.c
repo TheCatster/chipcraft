545da6d592262594907d79cb492624de906f716f
Daniil Rose <daniil.rose@posteo.org>
2023-11-25 22:03:52 -0500

    Update stack tests

--- tests/test_stack_new.c
+++ tests/test_stack_new.c
@@ -2,15 +2,15 @@
 // Created by Daniil Rose on 11/8/23.
 //
 
-#include "../include/chip8.h"
 #include <assert.h>
 #include <stdlib.h>
+#include "../include/chip8.h"
 
-int main() {
-    STACK *stack = stack_new();
-    assert(stack != NULL);
-    assert(stack->top == -1);
-    assert(stack->size == STACK_SIZE);
-    
-    return 0; // Success
+int main(void) {
+  STACK* stack = stack_new();
+  assert(stack != NULL);
+  assert(stack->top == -1);
+  assert(stack->size == STACK_SIZE);
+
+  return 0;  // Success
 }
--- tests/test_stack_pop.c
+++ tests/test_stack_pop.c
@@ -2,30 +2,30 @@
 // Created by Daniil Rose on 11/8/23.
 //
 
-#include "../include/chip8.h"
 #include <assert.h>
 #include <stdlib.h>
+#include "../include/chip8.h"
 
-int main() {
-    STACK *stack = stack_new();
-    uint16_t value;
-    bool result;
+int main(void) {
+  STACK* stack = stack_new();
+  uint16_t value;
+  bool result;
 
-    // Prepare the stack by pushing values.
-    for (uint16_t i = 0; i < STACK_SIZE; ++i) {
-        stack_push(stack, i);
-    }
+  // Prepare the stack by pushing values.
+  for (uint16_t i = 0; i < STACK_SIZE; ++i) {
+    stack_push(stack, i);
+  }
 
-    // Test popping values from the stack.
-    for (uint16_t i = STACK_SIZE; i > 0; --i) {
-        result = stack_pop(stack, &value);
-        assert(result);
-        assert(value == i - 1);
-    }
-
-    // Stack should be empty now.
+  // Test popping values from the stack.
+  for (uint16_t i = STACK_SIZE; i > 0; --i) {
     result = stack_pop(stack, &value);
-    assert(!result); // Popping from an empty stack should fail.
-    
-    return 0; // Success
+    assert(result);
+    assert(value == i - 1);
+  }
+
+  // Stack should be empty now.
+  result = stack_pop(stack, &value);
+  assert(!result);  // Popping from an empty stack should fail.
+
+  return 0;  // Success
 }
